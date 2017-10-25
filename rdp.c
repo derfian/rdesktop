db701d06a5fbf09d052bf7136343aaaaf464011a
Karl Mikaelsson <derfian@cendio.se>
2017-10-25 10:20:59 +0200

    Refactor process_save_session_info_pdu for different infoData
    The save session info PDU can contain several different kinds of data
    structures in the infoData field. The old code didn't do a great job
    of making this obvious, so I've substituted the if for a switch.
    
    I extracted the implementation of the only supported logon info
    structure to it's own function to make the code easier to read.
    
    Finally, I added a warning when the server sends infoData we haven't
    implemented support for.

--- rdp.c
+++ rdp.c
@@ -1442,6 +1442,51 @@ process_update_pdu(STREAM s)
 	ui_end_update();
 }
 
+/* Process a TS_LOGON_INFO_EXTENDED structure */
+static void
+process_logon_info_extended(STREAM s)
+{
+	uint32 fieldspresent;
+
+	in_uint8s(s, 2);	/* Length */
+	in_uint32_le(s, fieldspresent);
+	if (fieldspresent & LOGON_EX_AUTORECONNECTCOOKIE)
+	{
+		uint32 len;
+		uint32 version;
+
+		/* TS_LOGON_INFO_FIELD */
+		in_uint8s(s, 4);	/* cbFieldData */
+
+		/* ARC_SC_PRIVATE_PACKET */
+		in_uint32_le(s, len);
+		if (len != 28)
+		{
+			logger(Protocol, Error,
+			       "process_save_session_infotype_logon_extended_info(), "
+			       "invalid length in Auto-Reconnect packet");
+			return;
+		}
+
+		in_uint32_le(s, version);
+		if (version != 1)
+		{
+			logger(Protocol, Error,
+			       "process_save_session_infotype_logon_extended_info(), "
+			       "unsupported version of Auto-Reconnect packet");
+			return;
+		}
+
+		in_uint32_le(s, g_reconnect_logonid);
+		in_uint8a(s, g_reconnect_random, 16);
+		g_has_reconnect_random = True;
+		g_reconnect_random_ts = time(NULL);
+		logger(Protocol, Debug,
+		       "process_save_session_infotype_logon_extended_info(), "
+		       "saving Auto-Reconnect cookie, id=%u",
+		       g_reconnect_logonid);
+	}
+}
 
 /* Process a Save Session Info PDU */
 void
@@ -1449,46 +1494,19 @@ process_save_session_info_pdu(STREAM s)
 {
 	uint32 infotype;
 	in_uint32_le(s, infotype);
-	if (infotype == INFOTYPE_LOGON_EXTENDED_INFO)
+
+	switch (infotype)
 	{
-		uint32 fieldspresent;
-
-		in_uint8s(s, 2);	/* Length */
-		in_uint32_le(s, fieldspresent);
-		if (fieldspresent & LOGON_EX_AUTORECONNECTCOOKIE)
-		{
-			uint32 len;
-			uint32 version;
-
-			/* TS_LOGON_INFO_FIELD */
-			in_uint8s(s, 4);	/* cbFieldData */
-
-			/* ARC_SC_PRIVATE_PACKET */
-			in_uint32_le(s, len);
-			if (len != 28)
-			{
-				logger(Protocol, Error,
-				       "process_save_session_info_pdu(), invalid length in Auto-Reconnect packet");
-				return;
-			}
-
-			in_uint32_le(s, version);
-			if (version != 1)
-			{
-				logger(Protocol, Error,
-				       "process_save_session_info_pdu(), unsupported version of Auto-Reconnect packet");
-				return;
-			}
-
-			in_uint32_le(s, g_reconnect_logonid);
-			in_uint8a(s, g_reconnect_random, 16);
-			g_has_reconnect_random = True;
-			g_reconnect_random_ts = time(NULL);
-			logger(Protocol, Debug,
-			       "process_save_session_info_pdu(), saving Auto-Reconnect cookie, id=%u",
-			       g_reconnect_logonid);
-		}
+		case INFOTYPE_LOGON_EXTENDED_INFO:
+			process_logon_info_extended(s);
+			break;
+		default:
+			logger(Protocol, Warning,
+			       "process_save_session_info_pdu(), unhandled info type 0x%x",
+			       infotype);
+			break;
 	}
+
 }
 
 
